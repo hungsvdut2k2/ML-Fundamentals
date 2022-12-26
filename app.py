d40a3b0f27c9bbda803d58b8fe60f1db51d8bc0e
hungsvdut2k2 <hungsvdut@gmail.com>
2022-12-26 20:37:40 +0700

    feat: add logistic regression page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import linear_regression
 import index
+import logistic_regression
 
 Pages = {
     "Home": index,
-    "1.Linear Regression": linear_regression
+    "1.Linear Regression": linear_regression,
+    "2.Logistic Regression": logistic_regression
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(Pages.keys()))
